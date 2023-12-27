4804a71d6247a3de63505ebcf3f5abeb0a19e10c
vyjayanthiseshadri <vyjayanthiseshadri02@gmail.com>
2023-12-27 10:49:17 +0530

    Add files via upload

---
+++ environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAMN_ZMJxRopALfUUN86MHUyDkxGhw9niU",
+    authDomain: "latest-561ec.firebaseapp.com",
+    projectId: "latest-561ec",
+    storageBucket: "latest-561ec.appspot.com",
+    messagingSenderId: "137140511534",
+    appId: "1:137140511534:web:3aa35ed71ea18e2bd410bf"
+  },
+};
